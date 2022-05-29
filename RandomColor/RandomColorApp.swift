de97fed61e4851f34365363c059fec6dad9dd9bd
Sidma64 <sidma.ultra@gmail.com>
2022-05-30 00:42:26 +0300

    Initial Commit

---
+++ RandomColor/RandomColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomColorApp.swift
+//  RandomColor
+//
+//  Created by Uzay Toprak on 30.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct RandomColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
